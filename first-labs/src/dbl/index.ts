27ce34ebe289458b26ead1b70e33bc6ed9affc6a
Hat1ko <pasha1apple@gmail.com>
2020-11-14 20:49:18 +0200

    ⭐ feat(dbl): added skeleton structure for future dbl domains

--- first-labs/src/dbl/index.ts
+++ first-labs/src/dbl/index.ts
@@ -4,3 +4,9 @@ export * from './dbl.module'
 export * from './dbl.service'
 export * from './extended.repository'
 export * from './typeorm-module.provider'
+
+// Domains
+export * from './users'
+export * from './storages'
+export * from './categories'
+export * from './books'
